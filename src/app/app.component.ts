93752af831bfe5ef1b673446c92bb436f2b3efcf
usuwo-lumturo <hikaru.usuwo@gmail.com>
2020-06-22 20:08:44 +0900

    4 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+
+import { todoList } from './todoList';
+import { Todo } from './todo';
+
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+
+
 export class AppComponent  {
   title = 'My todo-list';
+  todoList: Todo[] = [...todoList];
 }
---
+++ src/app/spreadSyntax.ts
@@ -0,0 +1,5 @@
+const arr1 = [1, 2, 3];
+const arr2 = [...arr1, 4, 5, 6];
+
+console.log(arr2);
+// output: [1, 2, 3, 4, 5, 6]
